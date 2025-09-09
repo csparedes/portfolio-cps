1e3e467f7f7b5a66c7632f252c26e9b89f84a855
Cristian Paredes <csparedes1995@gmail.com>
2025-09-09 12:44:51 -0500

    ❇️ home page done

---
+++ app/components/CardWorkExp.vue
@@ -0,0 +1,36 @@
+<template>
+  <UCard variant="subtle">
+          <template #header>
+            <div class=" flex justify-between">
+              <div class="flex flex-col text-left">
+                <h2 class="text-lg font-bold">{{ title }}</h2>
+                <span class="text-sm font-light">{{ role }}</span>
+              </div>
+              <span class="text-sm font-light">{{ dateWork }}</span>
+            </div>
+          </template>
+          <div class="text-justify">{{ description }}</div>
+
+          <template #footer>
+            <div class="flex gap-2">
+              <span class="font-bold">Stack:</span>
+              <h6>{{ stack }}</h6>
+            </div>
+          </template>
+        </UCard>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  title: string,
+  role: string,
+  dateWork: string,
+  description: string,
+  stack: string,
+
+}>()
+</script>
+
+<style>
+
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="text-center">
-    
     <UContainer class="flex w-full justify-center"
       ><NuxtImg src="perfil.jpg" class="rounded-full w-60 h-60 p-4"
     /></UContainer>
@@ -9,17 +8,22 @@
     <div class="flex justify-center p-5">
       <div class="text-left">
         <h1 class="font-light text-lg">I'm Cristian Paredes Sánchez</h1>
-        <h2 class="uppercase font-bold text-lg">System Engineer / Software Developer</h2>
+        <h2 class="uppercase font-bold text-lg">
+          System Engineer / Software Developer
+        </h2>
       </div>
     </div>
 
     <div class="p-8">
-      <h1 class="text-left p-1">Have a look on my projects <NuxtLink to="/projects">here</NuxtLink></h1>
+      <h1 class="text-left p-1">
+        Have a look on my projects
+        <NuxtLink to="/projects" class="font-bold">here</NuxtLink>
+      </h1>
       <CarrouselImg :images="images" />
     </div>
 
     <div class="text-left p-1 md:p-8">
-      <h1 class="text-xl font-bold">About me</h1>
+      <h1 class="text-xl font-bold pb-4">About me</h1>
       <p>
         I improve software developing process and make tailor apps for solve
         your needs. Systems Engineer with a Master’s degree in Full Stack
@@ -34,12 +38,48 @@
       </p>
     </div>
     <div class="p-8">
-      <h3>Work experience</h3>
-      <ul>
-        <li>Pontificia Universidad Católica del Ecuador - Ibarra</li>
-        <li>Soluciones Tecnológicas Kauel - Chile</li>
-        <li>GAD Tulcán - Ecuador</li>
-      </ul>
+      <h3 class="text-left font-bold text-xl pb-4">Work experience</h3>
+
+      <div class="flex flex-col gap-8">
+        <CardWorkExp
+          title="Pontificia Universidad Católica del Ecuador - Ibarra"
+          role="System Analist, Fullstack Developer"
+          date-work="April 2022 - Current"
+          description="The Pontifical Catholic University of Ecuador - Ibarra is a renowned university in Ecuador, focused on education in values and the social and personal transformation of society. I currently work on academic software projects related to students, professors, and administrative staff, helping to automate their processes and streamline transactions."
+          stack="Vue, Nuxt, Postgres, Oracle, Docker, Jenkins"
+        />
+        
+        <CardWorkExp
+          title="Soluciones Tecnológicas Kauel - Remote, Chile"
+          role="Fullstack Developer"
+          date-work="November 2022 - April 2024"
+          description="Kauel Unique Solutions is a Chilean technology consulting firm
+            specializing in business solutions. During my time there, I led the
+            development of complex web applications for clients in the oil and
+            retail sectors."
+          stack="React, Nodejs, Figma, Azure, Sql Server, Postgres"
+
+        />
+
+        <CardWorkExp 
+          title="Intern: GAD Tulcán - Ecuador"
+          role="Intern: Database Manager Assistance"
+          date-work="January 2021 – March 2021"
+          description="The decentralized autonomous government of the city of Tulcán in Ecuador is a mayor's office responsible for administering all the services required by the city. During my internship, I assisted the Database Administrator of the Department of Popular Economy and Sustainable Development in gathering information on the city's markets for the creation of a consolidated database."
+          stack="Sql, MS Excel, Mysql"
+        />
+
+        <CardWorkExp 
+          title="Intern: Pontificia Universidad Católica del Ecuador - Ibarra",
+          role="Intern: IT Support and Virtualization"
+          date-work="May 2019 – March 2020"
+          description="As a student, I had the opportunity to intern in the infrastructure department at the Pontificia Universidad Católica del Ecuador Ibarra, where I assisted in the server virtualization process. This process includes everything from installing physical servers and configuring operating systems to routing and startup, in accordance with infrastructure guidelines."
+          stack="oVirt, Virtual Box, VmWare, Linux, Red Hat"
+
+        />
+
+        
+      </div>
     </div>
   </div>
 </template>
